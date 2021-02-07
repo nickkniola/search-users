e89c13ed1af455e05523d72627d51a72de12f506
Nicholas Kniola <nickkniola@me.com>
2021-02-07 00:37:47 +0000

    Add filter inputs with setCategory and setFilter functions

--- src/App.js
+++ src/App.js
@@ -1,7 +1,20 @@
 import logo from './logo.svg';
 import './App.css';
+import React, { useState } from 'react';
+import ReactDom from 'react-dom';
 
 function App() {
+  const [category, setCategory] = useState("name");
+  const [filter, setFilter] = useState("name")
+
+
+  const onCategoryChange = event => setCategory(event.target.value);
+
+  const handleFilter = event => {
+    event.preventDefault();
+    setFilter(category);
+  }
+
   return (
     <div className="App">
       <div className="top-bar">
@@ -14,10 +27,27 @@ function App() {
       </div> */}
       <div className="filter-view">
         <div className="filters">
-
+          <h2 className="header">Filters</h2>
+          <form onSubmit={handleFilter}>
+            <div>
+              <input type="radio" id="name" name="name" value="name" checked={category === "name"} onChange={onCategoryChange}/>
+              <label for="name">Name</label>
+            </div>
+            <div>
+              <input type="radio" id="age" name="age" value="age" checked={category === "age"} onChange={onCategoryChange}/>
+              <label for="age">Age</label>
+            </div>
+            <div>
+              <input type="radio" id="job" name="job" value="job" checked={category === "job"} onChange={onCategoryChange}/>
+              <label for="job">Job Title</label>
+            </div>
+            <div classNam="input-button-container">
+              <input type="submit" value="Apply"/>
+            </div>
+          </form>
         </div>
         <div className="results">
-
+          <h2 className="header">Results</h2>
         </div>
       </div>
     </div>
