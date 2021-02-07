fce3c40f3ecbc3ebc8a61f229cf61415a6c92b5d
Nicholas Kniola <nickkniola@me.com>
2021-02-07 02:10:10 +0000

    Complete backend data handling for POST express route and data rendering on front-end

--- server.js
+++ server.js
@@ -1,11 +1,25 @@
 const express = require('express');
-const bodyParser = require('body-parser')
 const path = require('path');
 const app = express();
+app.use(express.json());
 app.use(express.static(path.join(__dirname, 'build')));
 
-app.get('/ping', function (req, res) {
- return res.send('pong');
+const userData = [
+  { name: "Alex", age: "24", job: "Human Resources" },
+  { name: "Alex", age: "56", job: "President" },
+  { name: "Cody", age: "33", job: "IT" },
+  { name: "Joe", age: "45", job: "Accountant" },
+  { name: "Joe", age: "21", job: "Accountant" },
+  { name: "Sarah", age: "65", job: "Chief Financial Officer" },
+  { name: "Sarah", age: "24", job: "Human Resources" },
+  { name: "Sarah", age: "72", job: "IT" }
+];
+
+app.post('/search', function (req, res) {
+  console.log(req.body);
+  const filteredUsers = userData.filter(user => user.name === req.body.search)
+  console.log(filteredUsers)
+  return res.json(filteredUsers);
 });
 
 app.get('/', function (req, res) {
--- src/App.js
+++ src/App.js
@@ -7,21 +7,65 @@ function App() {
   const [view, setView] = useState("search-view");
   const [search, setSearch] = useState();
   const [category, setCategory] = useState("name");
-  const [filter, setFilter] = useState("name");
-
-
+  // const [filter, setFilter] = useState("name");
+  const [searchResult, setSearchResult] = useState();
+  const [sortFunction, setSortFunction] = useState();
 
   const onCategoryChange = event => setCategory(event.target.value);
 
   const handleFilter = event => {
     event.preventDefault();
-    setFilter(category);
+    // setFilter(category);
+    if (category === "name") {
+      setSortFunction(nameCompare);
+    } else if (category === "age") {
+      setSortFunction(ageCompare);
+    } else if (category === "job") {
+      setSortFunction(jobCompare);
+    }
   }
 
   const onSearchChange = event => setSearch(event.target.value);
 
   const handleSearch = () => {
-    setView("filter-view");
+    console.log(search);
+    fetch('/search', {
+      method: 'POST',
+      headers: {
+        'Content-type': 'application/json'
+      },
+      body: JSON.stringify({ search: search })
+    })
+      .then(response => response.json())
+      .then(data => setSearchResult(data))
+      .then(setView("filter-view"));
+  }
+
+  const nameCompare = (name1, name2) => {
+    if (name1.name < name2.name) {
+      return -1;
+    } else if (name2.name > name1.name) {
+      return 1;
+    }
+    return 0;
+  }
+
+  const jobCompare = (name1, name2) => {
+    if (name1.job < name2.job) {
+      return -1;
+    } else if (name2.job > name1.job) {
+      return 1;
+    }
+    return 0;
+  }
+
+  const ageCompare = (name1, name2) => {
+    if (name1.age < name2.age) {
+      return -1;
+    } else if (name2.age > name1.age) {
+      return 1;
+    }
+    return 0;
   }
 
   return (
@@ -41,15 +85,15 @@ function App() {
               <form onSubmit={handleFilter}>
                 <div>
                   <input type="radio" id="name" name="name" value="name" checked={category === "name"} onChange={onCategoryChange}/>
-                  <label for="name">Name</label>
+                  <label htmlFor="name">Name</label>
                 </div>
                 <div>
                   <input type="radio" id="age" name="age" value="age" checked={category === "age"} onChange={onCategoryChange}/>
-                  <label for="age">Age</label>
+                  <label htmlFor="age">Age</label>
                 </div>
                 <div>
                   <input type="radio" id="job" name="job" value="job" checked={category === "job"} onChange={onCategoryChange}/>
-                  <label for="job">Job Title</label>
+                  <label htmlFor="job">Job Title</label>
                 </div>
                 <div classNam="input-button-container">
                   <input type="submit" value="Apply"/>
@@ -58,6 +102,23 @@ function App() {
             </div>
             <div className="results">
               <h2 className="header">Results</h2>
+              {
+                searchResult ?
+                <div>
+                  {searchResult.sort(sortFunction).map(user => {
+                    return (
+                      <div className="users-result">
+                        <h3>Name: {user.name}</h3>
+                        <p>Age: {user.age}</p>
+                        <p>Job Title: {user.job}</p>
+                      </div>
+                      )
+                    })
+                  }
+                </div>
+                : <>
+                  </>
+              }
             </div>
           </div>
         }
